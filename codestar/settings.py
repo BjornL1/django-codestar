cc7cc3757a0ea0dd856113f220ac2311cb97e5c0
BjornL1 <>
2024-01-05 09:11:05 +0000

    Add secret key connection to files and heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$h^w))2r7c@5*or%db84mo0k-@(p5!17m2mx&w7ul7et=*nm2n'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -36,7 +36,907 @@ ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+DEBUG = False
+
+ALLOWED_HOSTS = ['8000-bjornl1-django-codestar-lt27kqcwv4.us2.codeanyapp.com',
+                 '.herokuapp.com']
+
+
+# Application definition
+
+INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
